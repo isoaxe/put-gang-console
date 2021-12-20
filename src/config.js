b6365fb76a9c2520c4afbe302f38c6f7ff4b0b5e
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-20 16:49:01 +0700

    config: Add firebaseConfig as comment
    Add the firebaseConfig object for put-gang but comment out for now. Will replace the MatX one when emulators running and populated with dummy data.

--- src/config.js
+++ src/config.js
@@ -13,3 +13,19 @@ export const auth0Config = {
     client_id: 'XmminWIs0S8gR3gIRBydYLWbF58x81vK',
     domain: 'matx.us.auth0.com',
 }
+
+// TODO: Replace the above MatX config object with the Put Gang one.
+// Note: Remove the databaseURL field before production release. Just dummy data, have not set up Realtime Database for the project, using Firestore instead.
+
+/*
+export var firebaseConfig = {
+    apiKey: "AIzaSyB6wghPLxzvAH7QCpkddSHM3zNlawiqdf8",
+    authDomain: "put-gang.firebaseapp.com",
+    databaseURL: 'https://matx-15ede.firebaseio.com', // Remove before prod release.
+    projectId: "put-gang",
+    storageBucket: "put-gang.appspot.com",
+    messagingSenderId: "1081481290914",
+    appId: "1:1081481290914:web:9d6e98abf85a63a95323fb",
+    measurementId: "G-B69GVNJBE9",
+};
+*/
