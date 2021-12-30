1994135170b8900a5fd716348a612b45dc65b5e7
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-30 08:15:37 +0700

    config: Switch firebaseConfig objects
    Delete the old MatX firebaseConfig object and replace with the Put Gang one. Still uses the databaseURL value from MatX for now though.
    Allows login and signup functionality to work with the Put Gang Firebase backend after this.

--- src/config.js
+++ src/config.js
@@ -1,23 +1,10 @@
-export var firebaseConfig = {
-    apiKey: 'AIzaSyBoUdiDtzQdC-m4nj9CPY5SvY7uKJGL71k',
-    authDomain: 'matx-15ede.firebaseapp.com',
-    databaseURL: 'https://matx-15ede.firebaseio.com',
-    projectId: 'matx-15ede',
-    storageBucket: 'matx-15ede.appspot.com',
-    messagingSenderId: '348111707030',
-    appId: '1:348111707030:web:70c4ca4eb3f1dbd18e1bb7',
-    measurementId: 'G-806629YLNN',
-}
-
 export const auth0Config = {
     client_id: 'XmminWIs0S8gR3gIRBydYLWbF58x81vK',
     domain: 'matx.us.auth0.com',
 }
 
-// TODO: Replace the above MatX config object with the Put Gang one.
-// Note: Remove the databaseURL field before production release. Just dummy data, have not set up Realtime Database for the project, using Firestore instead.
+// TODO: Remove the databaseURL field before production release. Just dummy data, have not set up Realtime Database for the project, using Firestore instead.
 
-/*
 export var firebaseConfig = {
     apiKey: "AIzaSyB6wghPLxzvAH7QCpkddSHM3zNlawiqdf8",
     authDomain: "put-gang.firebaseapp.com",
@@ -28,4 +15,3 @@ export var firebaseConfig = {
     appId: "1:1081481290914:web:9d6e98abf85a63a95323fb",
     measurementId: "G-B69GVNJBE9",
 };
-*/
