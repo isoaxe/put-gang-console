bd57b058ae2c2fdd14bb6858081f99e8fc84e55e
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-01 20:01:51 +0700

    authenticated: Convert to esm
    Change the module system from cjs to esm.

--- functions/auth/authenticated.js
+++ functions/auth/authenticated.js
@@ -1,7 +1,7 @@
-const admin = require("firebase-admin");
+import * as admin from "firebase-admin";
 
 
-exports.isAuthenticated = (req, res, next) => {
+export async function isAuthenticated (req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization)
@@ -17,15 +17,11 @@ exports.isAuthenticated = (req, res, next) => {
 	const token = split[1];
 
 	try {
-    // TODO: Check promise implementation here. resolve reject params?
-		const decodedToken = new Promise(() => {
-			admin.auth().verifyIdToken(token);
-		});
-    // TODO: Replace ES6 destructuring assignment below with CJS equivilent.
-		res.locals = { /* ...res.locals, */ uid: decodedToken.uid, bid: decodedToken.businessId, role: decodedToken.role, email: decodedToken.email };
+		const decodedToken = await admin.auth().verifyIdToken(token);
+		res.locals = {  ...res.locals, uid: decodedToken.uid, bid: decodedToken.businessId, role: decodedToken.role, email: decodedToken.email };
     /* The below success message is a temporary check for testing this file only. */
-		// return res.status(200).send({ message: "Success" });
-		return next();
+		return res.status(200).send({ message: "Success" });
+		// return next();
 	} catch (err) {
 		console.error(`${err.code} -  ${err.message}`);
 		return res.status(401).send({ message: "Authentication failure" });
