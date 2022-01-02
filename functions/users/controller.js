ca9fc1485b7e4c4c009317779d429f9e96e67df2
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-02 09:01:14 +0700

    controller: Add 'create' function
    Write a function that creates a new user and assigns the role of 'level-1' for now.
    Post data to Firestore too, will add more in a later commit.

--- functions/users/controller.js
+++ functions/users/controller.js
@@ -1,6 +1,35 @@
 import admin from "firebase-admin";
 
 
+// Create new level-1 user.
+export async function create (req, res) {
+	try {
+		const role = "level-1";
+		const { email, password } = req.body;
+
+		const { uid } = await admin.auth().createUser({
+			email,
+			password
+		});
+
+		await admin.auth().setCustomUserClaims(uid, { role });
+
+		// Not all required user data can be stored by auth. Use Firestore instead.
+		/* Note: Haven't added anything useful yet. That will come with hierarchy tree. */
+		const db = admin.firestore();
+		const user = db.collection("users").doc(uid);
+		user.set({
+			uid,
+			role
+		});
+
+		return res.status(200).send({ message: "level-1 user created" });
+	} catch (err) {
+		return handleError(res, err);
+	}
+}
+
+
 // Returns a list of all users.
 export async function all (req, res) {
 	try {
