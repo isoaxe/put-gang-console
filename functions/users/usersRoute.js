f41f3d43282cc921e3f856c13a8c759fc12e672b
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-02 07:37:50 +0700

    usersRoute: Add authorization to GET request
    Import the isAuthorized function. Then implement within the GET route to /users, allowing the top three roles access to all users for now.

--- functions/users/usersRoute.js
+++ functions/users/usersRoute.js
@@ -1,9 +1,11 @@
 import { isAuthenticated } from "./../auth/authenticated.js";
+import { isAuthorized } from "./../auth/authorized.js";
 
 
 export function usersRoute (app) {
 	// Fetch all users within the business.
 	app.get("/users",
-		isAuthenticated
+		isAuthenticated,
+		isAuthorized({ hasRole: ["admin", "level-1", "level-2"]})
 	);
 }
