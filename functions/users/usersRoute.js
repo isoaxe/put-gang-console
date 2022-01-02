32986403202e6ae1b5dd11b9207901d7ab93e228
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-02 08:05:34 +0700

    usersRoute: Add 'all' function
    Import and implement the 'all' function. This will return all user data after the auth steps have passed per the second last commit.

--- functions/users/usersRoute.js
+++ functions/users/usersRoute.js
@@ -1,3 +1,4 @@
+import { all } from "./controller.js";
 import { isAuthenticated } from "./../auth/authenticated.js";
 import { isAuthorized } from "./../auth/authorized.js";
 
@@ -6,6 +7,7 @@ export function usersRoute (app) {
 	// Fetch all users within the business.
 	app.get("/users",
 		isAuthenticated,
-		isAuthorized({ hasRole: ["admin", "level-1", "level-2"]})
+		isAuthorized({ hasRole: ["admin", "level-1", "level-2"]}),
+		all
 	);
 }
