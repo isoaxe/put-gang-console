c17eac3e0252e23f20c537f5e17bc572841ae2cf
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-07 21:09:16 +0700

    paymentsRoute: Add an 'init' route
    Add a route at the '/init' endpoint that runs the 'init' function after auth.

--- functions/payments/paymentsRoute.js
+++ functions/payments/paymentsRoute.js
@@ -1,9 +1,15 @@
-import { create, all } from "./controller.js";
+import { init, create, all } from "./controller.js";
 import { isAuthenticated } from "./../auth/authenticated.js";
 import { isAuthorized } from "./../auth/authorized.js";
 
 
 export default function paymentsRoute (app) {
+	// Initialize payments for a new user.
+	app.post("/payments/init/:uid",
+		isAuthenticated,
+		isAuthorized({ hasRole: ["admin", "level-1", "level-2", "level-3"]}),
+		init
+	);
 	// Create a new payment for a given user.
 	app.post("/payments/:uid/:type",
 		isAuthenticated,
