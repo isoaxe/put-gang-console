5e6415f5896f1e0779f21812df9044a1358011a2
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-07 21:05:02 +0700

    controller: Write init function
    Initialize all stats so they are zero. This way the 'create' function can add to them, knowing they are a number without checking first.
    This will reduce the size of the 'create' function that will be very large in any case. It is also more computationally efficient as checks no longer have to be run every time a request is made to 'create'.

--- functions/payments/controller.js
+++ functions/payments/controller.js
@@ -4,6 +4,24 @@ import admin from "firebase-admin";
 // Initialize payments for new user.
 export async function init (req, res) {
 	try {
+		const { uid } = req.params;
+		const { email } = req.body;
+		const db = admin.firestore();
+		const user = db.collection("payments").doc(uid);
+
+		// Initialize all stats.
+		const stats = user.collection("stats").doc("stats");
+		const [revenue, paid, unpaid, joinSold, watchSold] = Array(5).fill(0);
+		stats.set({
+			name: "",
+			email,
+			revenue,
+			paid,
+			unpaid,
+			joinSold,
+			watchSold
+		});
+
 		return res.status(200).send({ message: "Payments initialized for new user" });
 	} catch (err) {
 		return handleError(res, err);
