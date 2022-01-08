04ae46800b555421f34cd23351835b46e36df7d5
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-08 08:49:11 +0700

    Create constants.js
    Create a file that can keep constants. Add the ADMIN_UID here.

---
+++ functions/util/constants.js
@@ -0,0 +1,8 @@
+/*
+ * Constants that are used at several points throughout functions.
+ * Better to have a single point of reference that can changed as necessary.
+ */
+
+
+// User ID for admin.
+export const ADMIN_UID = "S9yKLsU2YtQHwly9jkFZNgdismQ2";
