26a1a70ac03b1471d04ba6aed2dfb2b3b70602f0
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-11 08:37:56 +0700

    controller: Initialize stats in 'create'
    First check if stats data for user exist (they will not if a new user). Then initialize stats if not found and user is senior (i.e. not level-3).
    It's better to automatically implement this here than call a separate api endpoint at the correct time.

--- functions/payments/controller.js
+++ functions/payments/controller.js
@@ -37,9 +37,26 @@ export async function create (req, res) {
 		const { type } = req.params;
 		const db = admin.firestore();
 
-		// Get current user data.
+		// Get current user and stats data.
 		const userRef = await db.collection("users").doc(uid).get();
 		const userData = userRef.data();
+		const stats = db.collection("payments").doc(uid).collection("stats").doc("stats");
+		const statsRef = await stats.get();
+		const statsData = statsRef.data()
+
+		if (!statsData && ["admin", "level-1", "level-2"].includes(role)) {
+			// Initialize all stats. These data relate to the user's earnings from their downline.
+			const [revenue, paid, unpaid, sales, invoiceId] = Array(5).fill(0);
+			stats.set({
+				name: "",
+				email,
+				revenue,
+				paid,
+				unpaid,
+				sales,
+				invoiceId
+			});
+		}
 
 		// Get admin stats as payments from all users will accrue here.
 		const adminUser = db.collection("payments").doc(ADMIN_UID);
