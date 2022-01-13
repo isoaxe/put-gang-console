f7a96db84d0534bda419400ecfd3bd68b2711a61
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-13 20:44:19 +0700

    controller: Add template 'create' and 'all' functions
    Declare the two functions with a try / catch block and error handling. Just return a placeholder message for now. Will build the functions in subsequent commits.

---
+++ functions/activity/controller.js
@@ -0,0 +1,28 @@
+import admin from "firebase-admin";
+
+
+// Create a new activity.
+export async function create (req, res) {
+	try {
+		const { type } = req.params
+		return res.status(200).send({ message: "placeholder message" });
+	} catch (err) {
+		return handleError(res, err);
+	}
+}
+
+
+// Get all activity applicable to the calling user.
+export async function all (req, res) {
+	try {
+		return res.status(200).send({ message: "placeholder message"});
+	} catch (err) {
+		return handleError(res, err);
+	}
+}
+
+
+// Standard error helper function.
+function handleError (res, err) {
+	return res.status(500).send({ error: `${err}` });
+}
