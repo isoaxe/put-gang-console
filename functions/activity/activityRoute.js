cfa38c56405d2867806b82dc9572d902d7502816
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-13 21:25:39 +0700

    activityRoute: Add params
    + Change the param name to 'action' as it describes it better. This would be 'join', 'cancel' or some other verb.
    + Add a new param for 'product'. This could be subscription type such as 'watch', 'join' or 'news'.

--- functions/activity/activityRoute.js
+++ functions/activity/activityRoute.js
@@ -5,7 +5,7 @@ import { isAuthorized } from "./../auth/authorized.js";
 
 export default function activityRoute (app) {
 	// Create a new activity.
-	app.post("/activity/:type",
+	app.post("/activity/:action/:product",
 		isAuthenticated,
 		isAuthorized({ hasRole: ["admin", "level-1", "level-2", "level-3", "standard"] }),
 		create
