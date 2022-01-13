e46845d4db9e7dc5e2515c6c8ff564f867970617
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-13 21:27:14 +0700

    controller: Add locals, params and db to 'create'
    + Extract variables from locals and params as with payment route.
    + Initialize the database with a call to Firestore.

--- functions/activity/controller.js
+++ functions/activity/controller.js
@@ -4,7 +4,10 @@ import admin from "firebase-admin";
 // Create a new activity.
 export async function create (req, res) {
 	try {
-		const { type } = req.params
+		const { uid, role, email } = res.locals;
+		const { action, product } = req.params;
+		const db = admin.firestore();
+
 		return res.status(200).send({ message: "placeholder message" });
 	} catch (err) {
 		return handleError(res, err);
