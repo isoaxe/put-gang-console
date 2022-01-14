c1dbe3f1ce21ce4dcdfb2b788b5f9ba62d5da284
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-14 16:50:48 +0700

    helpers: Add filterObject function
    Add a helper that filters a nested object based on a specified nested key and allowed values for that key.
    This is what's needed for filtering the activities object based on allowed user ids for level-1 and level-2 users.

--- functions/util/helpers.js
+++ functions/util/helpers.js
@@ -13,3 +13,26 @@ export function addMonth (date) {
 export function newSubscriber (alreadySubbed, paymentType) {
   return !alreadySubbed && (paymentType === "join" || paymentType === "watch");
 }
+
+
+/* Filter a nested object based on an array of authorized values.
+ * The values in the object will themselves be objects.
+ * The filtered values will be inside the value object, hence nested.
+ * const obj = {
+ *  "4": { "key1": "value1", "name": "peter" },
+ *  "2": { "key2": "value2", "name": "paul" },
+ *  "8": { "key3": "value3", "name": "jacob" }
+ * }
+ *
+ * Can filter with an array of name values, e.g. [peter, jacob].
+ */
+
+ export function filterObject (obj, allowed, targetKey) {
+   const filter = (obj, predicate) =>
+     Object.assign(
+       ...Object.keys(obj)
+       .filter( key => predicate(obj[key]) )
+       .map( key => ({ [key]: obj[key] }) )
+     );
+   return filter(obj, data => allowed.includes(data[targetKey]));
+ }
