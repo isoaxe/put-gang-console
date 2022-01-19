2f39e5199488ed7c351a238aa295a250c9918f2e
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-19 14:48:03 +0700

    helpers: Add getData function
    Make a generic getData function that makes a GET request to Firestore.
    Takes an api endpoint as a param along with a setter method to store fetched data to state.
    Needed all the more as modularization of data fetching will lead to many requests. Keeps code DRY.

--- src/app/utils/helpers.js
+++ src/app/utils/helpers.js
@@ -1,6 +1,8 @@
 /*
  * Various helper functions used throughout the project.
  */
+import firebase from 'firebase/app';
+import { API_URL } from './urls';
 
 
 // Turns a compound object into an array of objects.
@@ -14,4 +16,24 @@ export function objectToArray (compoundObj) {
     result.push(obj);
   })
   return result;
+}
+
+
+// Get data from Firestore at given endpoint and save to state.
+export async function getData (endpoint, setterFunction) {
+  const token = await firebase.auth().currentUser.getIdToken(true);
+  const fetchConfig = {
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  };
+  const response = await fetch(API_URL + endpoint, fetchConfig);
+  const jsonResponse = await response.json();
+  setterFunction(jsonResponse); // Stores data to state.
+  if (jsonResponse.error) {
+    console.error(jsonResponse);
+  }
 }
