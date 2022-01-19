02d719afb4453752e3c2310214c30aef7cfaadc9
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-19 07:43:40 +0700

    LoadData: Fetch payments data
    + Add back all the logic that was removed from <Console/> in the previous commit.
    + Pass the stats data and role value to all components below via an object with the context provider.

--- src/app/LoadData.jsx
+++ src/app/LoadData.jsx
@@ -1,15 +1,55 @@
-import '../fake-db'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import firebase from 'firebase/app';
 import { useRoutes } from 'react-router-dom'
 import DataContext from './contexts/DataContext'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import useAuth from './hooks/useAuth';
 import { AllPages } from './routes/routes'
+import { API_URL } from './utils/urls';
 
 const LoadData = () => {
+    const [payments, setPayments] = useState({});
+    const [stats, setStats] = useState({});
+    const [invoices, setInvoices] = useState({});
+    const [role, setRole] = useState("");
     const all_pages = useRoutes(AllPages())
+    const { user } = useAuth();
+    const uid = user.id;
+
+    async function getPayments () {
+      const user = firebase.auth().currentUser;
+      const token = await user.getIdToken(true);
+      const result = await user.getIdTokenResult(true);
+      setRole(result.claims.role);
+      const fetchConfig = {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      };
+      const response = await fetch(`${API_URL}/payments`, fetchConfig);
+      const jsonResponse = await response.json();
+      setPayments(jsonResponse);
+      if (jsonResponse.error) {
+        console.log(jsonResponse);
+      }
+    }
+
+    useEffect(() => {
+      getPayments();
+    }, [])
+
+    useEffect(() => {
+      if (Object.keys(payments).length) {
+        setStats(payments[uid].stats);
+        setInvoices(payments[uid].invoices);
+      }
+    }, [payments, uid]);
 
     return (
-        <DataContext.Provider>
+        <DataContext.Provider value={{stats, role}}>
             {all_pages}
             <Routes>
                 <Route path='/' element={<Navigate to="/dashboard/default" />} />
