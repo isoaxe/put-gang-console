bafc35a01d855fc84404f94b3827c52618debd5e
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-19 14:49:46 +0700

    LoadData: Replace getActivity with helper
    Replace the getActivity function with a call to the generic getData. Much more concise.

--- src/app/LoadData.jsx
+++ src/app/LoadData.jsx
@@ -5,6 +5,7 @@ import DataContext from './contexts/DataContext'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import useAuth from './hooks/useAuth';
 import { AllPages } from './routes/routes'
+import { getData } from './utils/helpers';
 import { API_URL } from './utils/urls';
 
 const LoadData = () => {
@@ -18,23 +19,7 @@ const LoadData = () => {
     const { user } = useAuth();
     const uid = user.id;
 
-    async function getActivity () {
-      const token = await firebase.auth().currentUser.getIdToken(true);
-      const fetchConfig = {
-        method: "GET",
-        headers: {
-          authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        }
-      };
-      const response = await fetch(`${API_URL}/activity`, fetchConfig);
-      const jsonResponse = await response.json();
-      setActivities(jsonResponse);
-      if (jsonResponse.error) {
-        console.log(jsonResponse);
-      }
-    }
+    const getActivity = () => getData("/activity", setActivities);
 
     async function getPayments () {
       const user = firebase.auth().currentUser;
