19ae613355e81f7c41be8bcb23ef7dc86e12a0df
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-24 14:34:29 +0700

    helpers: Add the formatReceiptStatement
    Add a function that was previously declared in ActivityListView.
    It will be used in more than one location so this keeps things DRY.

--- src/app/utils/helpers.js
+++ src/app/utils/helpers.js
@@ -40,3 +40,15 @@ export async function getData (endpoint, setterFunction) {
   }
   return jsonResponse;
 }
+
+
+// Form a statement for each receipt based on data.
+export function formatReceiptStatement (name, email, action, product, sale) {
+  let productStatement, actionStatement;
+  if (action === "join") actionStatement = "has subscribed to";
+  if (action === "payment") actionStatement = `has made a $${sale} payment for`;
+  if (action === "cancel") actionStatement = "has cancelled their subscription to"
+  if (product === "join") productStatement = "Join the Discussion";
+  if (product === "watch") productStatement = "Watch the Discussion";
+  return `${name ? name : email} ${actionStatement} ${productStatement}.`
+}
