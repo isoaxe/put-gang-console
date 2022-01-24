5f71e33bab287cf9b411016e11ec49f878bef8ba
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-24 15:22:21 +0700

    Users: Remove unused code
    Strip out most of the component, mainly just keeping the <UserList/> and title with date sorter.

--- src/app/views/dashboard/Users.jsx
+++ src/app/views/dashboard/Users.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import ActivityList from './../list/ActivityList';
-import StatCard3 from './shared/StatCard3'
-import StatCard4 from './shared/StatCard4'
-import FollowerCard from './shared/FollowerCard'
-import FollowerCard2 from './shared/FollowerCard2'
-import ComparisonChart2 from './shared/ComparisonChart2'
-import GaugeProgressCard from './shared/GuageProgressCard'
-import { H3, Span } from './../../components/Typography';
-import { styled, useTheme } from '@mui/system'
+import UserList from './../list/UserList';
+import { H3 } from './../../components/Typography';
+import { styled } from '@mui/system'
 import {
-    Card,
     TextField,
     MenuItem,
-    IconButton,
-    Icon,
-    Grid,
 } from '@mui/material'
 
 const AnalyticsRoot = styled('div')(({ theme }) => ({
@@ -32,13 +22,11 @@ const FlexBox = styled('div')(({ theme }) => ({
 }))
 
 const Console = () => {
-    const { palette } = useTheme();
-    const textMuted = palette.text.secondary;
 
     return (
         <AnalyticsRoot>
             <FlexBox>
-                <H3 sx={{ m: 0 }}>Overview</H3>
+                <H3 sx={{ m: 0 }}>Users</H3>
                 <TextField
                     defaultValue="1"
                     variant="outlined"
@@ -52,41 +40,7 @@ const Console = () => {
                 </TextField>
             </FlexBox>
 
-            <StatCard3 />
-
-            <H3 sx={{ marginTop: 8 }}>Activity</H3>
-            <ActivityList />
-
-            <Card sx={{ mt: '20px', mb: '24px' }} elevation={3}>
-                <FlexBox
-                    sx={{
-                        px: 2,
-                        py: '12px',
-                        background: 'rgba(0, 0, 0, 0.01)',
-                    }}
-                >
-                    <Span sx={{ fontWeight: '500', color: textMuted }}>
-                        STATISTICS
-                    </Span>
-                    <IconButton size="small">
-                        <Icon>more_horiz</Icon>
-                    </IconButton>
-                </FlexBox>
-                <ComparisonChart2 height={400} />
-            </Card>
-
-            <Grid container spacing={3}>
-                <Grid item md={4} xs={12}>
-                    <StatCard4 />
-                </Grid>
-                <Grid item md={4} xs={12}>
-                    <GaugeProgressCard />
-                </Grid>
-                <Grid item md={4} xs={12}>
-                    <FollowerCard />
-                    <FollowerCard2 />
-                </Grid>
-            </Grid>
+            <UserList />
         </AnalyticsRoot>
     )
 }
