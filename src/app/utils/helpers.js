334d5539f6ad3fa2583a0c9e0e117d2114254ffd
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-26 20:50:22 +0700

    helpers: Remove objectToArray function
    This function is now redundant. Where an array is required from the backend, it is formed there.
    This saves conversion at the frontend with its unnecessary complexity and pointless computational overhead.

--- src/app/utils/helpers.js
+++ src/app/utils/helpers.js
@@ -5,20 +5,6 @@ import firebase from 'firebase/app';
 import { API_URL } from './urls';
 
 
-// Turns a compound object into an array of objects.
-// Each object in the array is the value from original with key inserted.
-export function objectToArray (compoundObj) {
-  const result = [];
-  const keys = Object.keys(compoundObj);
-  keys.forEach(key => {
-    const obj = compoundObj[key]; // Nested object is value.
-    obj["id"] = key; // Add key to object.
-    result.push(obj);
-  })
-  return result;
-}
-
-
 // Get data from Firestore at given endpoint and optionally save to state.
 export async function getData (endpoint, setterFunction) {
   const token = await firebase.auth().currentUser.getIdToken(true);
