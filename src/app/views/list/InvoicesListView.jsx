f0e08895ae7ae3519d6c6b250672dad9043a28c7
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-30 11:00:28 +0700

    Create InvoicesListView.jsx
    This is a copy of <ActivityListView/>. Only differences are the removal of ScrollBar, rearranging of import statements and component name.

---
+++ src/app/views/list/InvoicesListView.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Card, Avatar, Grid } from '@mui/material';
+import { Box, styled, useTheme } from '@mui/system'
+import ReceiptsModal from './../modal/ReceiptsModal';
+import { displayReceipts } from './../../utils/helpers';
+import { Small, Span, Paragraph } from 'app/components/Typography';
+import { themeShadows } from 'app/components/MatxTheme/themeColors';
+import {
+  AddTask,
+  HighlightOff,
+  MailOutline,
+  MonetizationOn
+} from '@mui/icons-material';
+
+
+const FlexBox = styled(Box)(() => ({
+    display: 'flex',
+    alignItems: 'center',
+}))
+
+const ListCard = styled(Card)(({ theme }) => ({
+    padding: '8px',
+    position: 'relative',
+    transition: 'all 0.3s ease',
+    boxShadow: themeShadows[12],
+    '& .card__button-group': {
+        display: 'none',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        zIndex: 1,
+        background: theme.palette.background.paper,
+    },
+    '&:hover': {
+        cursor: 'pointer',
+        '& .card__button-group': {
+            display: 'flex',
+            alignItems: 'center',
+        },
+    },
+}))
+
+function actionImage (action, product) {
+  if (action === "join" && product === "news") return <MailOutline color="info" />;
+  if (action === "cancel" && product === "news") return <MailOutline color="disabled" />;
+  if (action === "join") return <AddTask color="success" />;
+  if (action === "cancel") return <HighlightOff color="error" />;
+  if (action === "recur") return <MonetizationOn color="success" />;
+}
+
+const InvoicesListView = ({ list = [] }) => {
+    const [visible, setVisible] = useState(false);
+    const [receipts, setReceipts] = useState([]);
+    const { palette } = useTheme();
+    const textMuted = palette.text.secondary;
+
+    return (
+        <div>
+            {list.map((item, index) => (
+                <ListCard
+                    key={item.id}
+                    elevation={3}
+                    sx={{ mb: index < list.length && 2 }}
+                    onClick={() => displayReceipts(item.uid, setReceipts, setVisible)}
+                >
+                    <Grid container justify="space-between" alignItems="center">
+                        <Grid item md={10}>
+                            <FlexBox>
+                                {actionImage(item.action, item.product)}
+                                <Box ml={2}>
+                                    <Paragraph sx={{ mb: 1 }}>
+                                        {item.statement}
+                                    </Paragraph>
+                                    <Box display="flex">
+                                        <Small sx={{ color: textMuted }}>
+                                            {new Date(item.date).toLocaleString()}
+                                        </Small>
+                                        <Small sx={{ ml: 3, color: textMuted }}>
+                                            {item.email}
+                                        </Small>
+                                    </Box>
+                                </Box>
+                            </FlexBox>
+                        </Grid>
+                        <Grid item md={2}>
+                            <FlexBox>
+                                <Avatar src={item.userImage}></Avatar>
+                                <Span sx={{ ml: '16px' }}>{item.userName}</Span>
+                            </FlexBox>
+                        </Grid>
+                    </Grid>
+                </ListCard>
+            ))}
+            <ReceiptsModal
+								visible={visible}
+								setVisible={setVisible}
+                receipts={receipts}
+							/>
+        </div>
+    )
+}
+
+export default InvoicesListView;
