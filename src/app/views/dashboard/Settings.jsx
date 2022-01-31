9efc651afba500b23d400f96d8f00f8788a30c2b
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-01-31 21:32:22 +0700

    Settings: Add name field
    + Add a MUI TextField for inputting the users name. This will eventually be saved to Firestore.
    + Add a heading to the page.

--- src/app/views/dashboard/Settings.jsx
+++ src/app/views/dashboard/Settings.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { styled } from '@mui/system';
-import { H5 } from 'app/components/Typography';
+import { styled, useTheme } from '@mui/system';
+import { TextField } from '@mui/material';
+import { H3, H5 } from 'app/components/Typography';
 import useAuth from './../../hooks/useAuth';
 
 
@@ -11,14 +12,27 @@ const Container = styled('div')(({ theme }) => ({
     },
 }))
 
-const Settings = () => {
-    const uid = useAuth().user.id;
+const FlexBox = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: '24px',
+}))
 
-    return (
-        <Container>
-          <button/>
-        </Container>
-    )
+const Settings = () => {
+  const { palette } = useTheme();
+  const textMuted = palette.text.secondary;
+  const uid = useAuth().user.id;
+
+  return (
+    <Container>
+      <H3 sx={{ marginBottom: "2rem" }}>Settings</H3>
+        <H5 sx={{ marginBottom: "10px", color: textMuted }}>
+          Current Name:
+        </H5>
+        <TextField label="Name" />
+    </Container>
+  )
 }
 
 export default Settings;
