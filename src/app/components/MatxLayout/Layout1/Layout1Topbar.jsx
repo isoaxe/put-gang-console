bacb019b880b8ea7549e404401231d8700d8e28a
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-03 20:23:40 +0700

    Layout1Topbar: Remove icons on left
    Remove the 3 buttons at the left side of the top bar. The only one remaining is the one that fully collapses the side bar.

--- src/app/components/MatxLayout/Layout1/Layout1Topbar.jsx
+++ src/app/components/MatxLayout/Layout1/Layout1Topbar.jsx
@@ -123,20 +123,6 @@ const Layout1Topbar = () => {
                     <StyledIconButton onClick={handleSidebarToggle}>
                         <Icon>menu</Icon>
                     </StyledIconButton>
-
-                    <IconBox>
-                        <StyledIconButton>
-                            <Icon>mail_outline</Icon>
-                        </StyledIconButton>
-
-                        <StyledIconButton>
-                            <Icon>web_asset</Icon>
-                        </StyledIconButton>
-
-                        <StyledIconButton>
-                            <Icon>star_outline</Icon>
-                        </StyledIconButton>
-                    </IconBox>
                 </Box>
                 <Box display="flex" alignItems="center">
                     <MatxSearchBox />
