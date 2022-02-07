52f23c60c4f0d64ff44158e59a5aec4fa74f8ac5
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-07 15:48:28 +0700

    helpers: Add getAvatarUrl function
    Move the function that was previously in 'edit' to helpers.
    There will be several functions relating to the fetching and storing of the Instagram profile photo. Too may to keep in the 'edit' function.

--- functions/util/helpers.js
+++ functions/util/helpers.js
@@ -1,6 +1,7 @@
 /*
  * Various helper functions used throughout the project.
  */
+ import fetch from "node-fetch";
 
 
 // Add 31 days to the supplied date.
@@ -12,4 +13,24 @@ export function addMonth (date) {
 // Returns true if user has not subscribed before and payment is subscription type.
 export function newSubscriber (alreadySubbed, paymentType) {
   return !alreadySubbed && (paymentType === "join" || paymentType === "watch");
+}
+
+
+
+/*
+ *   Helper functions for the above helpers.
+ *   As such, these do not get exported.
+ */
+
+async function getAvatarUrl(username) {
+  const fetchConfig = {
+    method: "GET",
+    headers: {
+      host: "www.instagram.com"
+    }
+  };
+  const response = await fetch(`https://www.instagram.com/${username}/?__a=1`, fetchConfig);
+  const jsonResponse = await response.json();
+  const photoUrl = jsonResponse.graphql?.user?.profile_pic_url_hd;
+  return photoUrl;
 }
