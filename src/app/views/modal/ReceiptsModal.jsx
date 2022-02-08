8272bef874640ef729e42a11b6f6be6134dcb680
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-08 19:25:22 +0700

    ReceiptsModal: Add avatar to modal
    + Style the Avatar so that it is larger.
    + Wrap the Avatar in a styled Box component to center it horizontally.
    + Extract avatarUrl and pass to Avatar.
    => Avatar now displayed on payments modal when navigated to from Users or ActivityListView.

--- src/app/views/modal/ReceiptsModal.jsx
+++ src/app/views/modal/ReceiptsModal.jsx
@@ -1,12 +1,20 @@
 import Modal from "react-modal";
+import { Avatar } from '@mui/material';
+import { Box, styled } from '@mui/system';
 import { H2, H3 } from 'app/components/Typography';
 import ReceiptsListView from "./../list/ReceiptsListView";
 import "./ReceiptsModal.css";
 
 
+const FlexBox = styled(Box)(() => ({
+		display: 'flex',
+		justifyContent: 'center',
+}));
+
 function ReceiptsModal (props) {
 	Modal.setAppElement("#root");
-	const { name, email } = props.selectedUser;
+	const { name, email, avatarUrl } = props.selectedUser;
+	const avatarStyles = { width: '130px', height: '130px' };
 
 	function close () {
 		props.setVisible(false);
@@ -30,6 +38,9 @@ function ReceiptsModal (props) {
 		>
 			<div>
 				<H2>Payment History</H2>
+				<FlexBox>
+					<Avatar sx={avatarStyles} src={avatarUrl}></Avatar>
+				</FlexBox>
 				<H3>{name || email}</H3>
 				<H3>Total spent: ${totalSpent()}</H3>
 				<ReceiptsListView receipts={props.receipts} />
