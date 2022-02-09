9fdf5b70c2bcafad72cc5a470ad3c0493cf1965d
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-09 21:35:56 +0700

    routes: Remove unneeded routes
    Remove the majority of the routes. There may be more to go, but that should be most of them.

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -1,22 +1,10 @@
 import AuthGuard from "app/auth/AuthGuard";
 import NotFound from "app/views/sessions/NotFound";
 import chartsRoute from "app/views/charts/ChartsRoute";
-import chatRoutes from "app/views/chat-box/ChatRoutes";
-import crudRoute from "app/views/CRUD/CrudRoutes";
-import formsRoutes from "app/views/forms/FormsRoutes";
 import ListRoute from "app/views/list/ListRoute";
-import mapRoutes from "app/views/map/MapRoutes";
-import pagesRoutes from "app/views/pages/pagesRoutes";
-import todoRoutes from "app/views/todo/TodoRoutes";
-import inboxRoute from "app/views/inbox/InboxRoutes";
-import pricingRoutes from "app/views/pricing/PricingRoutes";
-import invoiceRoutes from "app/views/invoice/InvoioceRoutes";
-import calendarRoutes from "app/views/calendar/CalendarRoutes";
-import ecommerceRoutes from "app/views/ecommerce/EcommerceRoutes";
 import dataTableRoutes from "app/views/data-table/dataTableRoutes";
 import materialRoutes from "app/views/material-kit/MaterialRoutes";
 import dragAndDropRoute from "app/views/Drag&Drop/DragAndDropRoute";
-import scrumBoardRoutes from "app/views/scrum-board/ScrumBoardRoutes";
 import pageLayoutRoutes from "app/views/page-layouts/PageLayoutRoutees";
 import { dashboardRoutes } from "app/views/dashboard/DashboardRoutes";
 import sessionRoutes from "app/views/sessions/SessionRoutes";
@@ -33,24 +21,12 @@ export const AllPages = () => {
       ),
       children: [
         ...dashboardRoutes,
-        ...calendarRoutes,
         ...chartsRoute,
-        ...chatRoutes,
-        ...crudRoute,
         ...dataTableRoutes,
         ...dragAndDropRoute,
-        ...ecommerceRoutes,
-        ...formsRoutes,
-        ...invoiceRoutes,
         ...ListRoute,
-        ...mapRoutes,
         ...materialRoutes,
-        ...inboxRoute,
         ...pageLayoutRoutes,
-        ...pagesRoutes,
-        ...pricingRoutes,
-        ...scrumBoardRoutes,
-        ...todoRoutes,
       ],
     },
     ...sessionRoutes,
