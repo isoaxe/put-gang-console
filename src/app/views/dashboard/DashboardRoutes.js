4718bbac2ff1f7ea41210087422892a27f5da673
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-09 20:10:03 +0700

    DashboardRoutes: Remove unneeded routes
    Remove the routes that came with the MatX template.

--- src/app/views/dashboard/DashboardRoutes.js
+++ src/app/views/dashboard/DashboardRoutes.js
@@ -1,16 +1,12 @@
 import React, { lazy } from 'react'
 import Loadable from 'app/components/Loadable/Loadable';
-
-const Analytics = Loadable(lazy(() => import("./Analytics")));
-const Analytics2 = Loadable(lazy(() => import("./Analytics2")));
-const Analytics3 = Loadable(lazy(() => import("./Analytics3")));
 const Console = Loadable(lazy(() => import("./Console")));
 const Users = Loadable(lazy(() => import("./Users")));
 const Affiliates = Loadable(lazy(() => import("./Affiliates")));
 const Affiliate = Loadable(lazy(() => import("./Affiliate")));
 const Links = Loadable(lazy(() => import("./Links")));
 const Settings = Loadable(lazy(() => import("./Settings")));
-const InventoryManagement = Loadable(lazy(() => import("./InventoryManagement")));
+
 
 export const dashboardRoutes = [
     {
@@ -36,21 +32,5 @@ export const dashboardRoutes = [
     {
         path: '/dashboard/settings',
         element: <Settings />,
-    },
-    {
-        path: '/dashboard/default',
-        element: <Analytics2 />,
-    },
-    {
-        path: '/dashboard/analytics',
-        element: <Analytics3 />,
-    },
-    {
-        path: '/dashboard/alternative',
-        element: <Analytics />,
-    },
-    {
-        path: '/dashboard/inventory-management',
-        element: <InventoryManagement />,
     },
 ]
