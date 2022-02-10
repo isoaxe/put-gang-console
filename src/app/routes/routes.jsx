be5b525f40b4d36cd4fdff84f5d24457a9420e0f
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-10 10:41:42 +0700

    routes: Remove further unneeded routes
    During the deletion of views, some further routes were identified for removal.

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -2,10 +2,6 @@ import AuthGuard from "app/auth/AuthGuard";
 import NotFound from "app/views/sessions/NotFound";
 import chartsRoute from "app/views/charts/ChartsRoute";
 import ListRoute from "app/views/list/ListRoute";
-import dataTableRoutes from "app/views/data-table/dataTableRoutes";
-import materialRoutes from "app/views/material-kit/MaterialRoutes";
-import dragAndDropRoute from "app/views/Drag&Drop/DragAndDropRoute";
-import pageLayoutRoutes from "app/views/page-layouts/PageLayoutRoutees";
 import { dashboardRoutes } from "app/views/dashboard/DashboardRoutes";
 import sessionRoutes from "app/views/sessions/SessionRoutes";
 import MatxLayout from '../components/MatxLayout/MatxLayout'
@@ -22,11 +18,7 @@ export const AllPages = () => {
       children: [
         ...dashboardRoutes,
         ...chartsRoute,
-        ...dataTableRoutes,
-        ...dragAndDropRoute,
         ...ListRoute,
-        ...materialRoutes,
-        ...pageLayoutRoutes,
       ],
     },
     ...sessionRoutes,
