d95847c784ae776efb962cd5261d5d35ea09d0c8
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-11 13:23:01 +0700

    instagramAvatar: Remove functions from prior Instagram api attempt
    Remove the getAvatar function and it's four helpers from this file. These were all the functions associated with the previous attempt to get the Instagram api working on a prior branch.
    The problem with the previous solution was that it did not work in the remote environment. The new solution uses programmatic login to Instagram and CSRF tokens to gain access.

--- functions/users/instagramAvatar.js
+++ functions/users/instagramAvatar.js
@@ -1,6 +1,5 @@
 import Firestore from "@google-cloud/firestore";
 import { Storage } from "@google-cloud/storage";
-import admin from "firebase-admin";
 import fetch from "node-fetch";
 
 
@@ -61,79 +60,12 @@ export async function storeProfilePic (user) {
   }
 }
 
-// Checks to see if photo is already in Firebase Storage first. If not, retrieve
-// url from Instagram, save photo to Storage and return that url.
-// This prevents excessive calls and avoids Instagram blocking our requests.
-export async function getAvatar (username) {
-  let url = null;
-  let from_cache = false;
-
-  const cache = await getAvatarFromCache(username);
-  if (cache.exists) {
-    url = cache.url;
-    from_cache = true;
-  } else {
-    const picUrl = await getAvatarUrl(username);
-    const imgBuffer = await getAvatarImage(picUrl);
-    url = await uploadAvatar(imgBuffer, username);
-  }
-  return { url, from_cache };
-}
-
 
 
 /*
  *   Helper functions for the above function.
  *   As such, these do not get exported.
  */
-
-// Returns the users instagram profile photo from the Public api.
-async function getAvatarUrl (username) {
-  const fetchConfig = {
-    method: "GET",
-    headers: {
-      host: "www.instagram.com"
-    }
-  };
-  const response = await fetch(`https://www.instagram.com/${username}/?__a=1`, fetchConfig);
-  const jsonResponse = await response.json();
-  const photoUrl = jsonResponse.graphql?.user?.profile_pic_url_hd;
-  return photoUrl;
-}
-
-// Return the profile photo at the provided url.
-async function getAvatarImage (url) {
-  let response = await fetch(url);
-  let data = await response.arrayBuffer();
-  const buffer = Buffer.from(data);
-  return buffer;
-}
-
-// Uploads the profile photo to a Firebase storage bucket.
-async function uploadAvatar (imageBuffer, username) {
-  const storage = admin.storage().bucket(bucketId);
-  const file = storage.file(`${bucketPath}/${username}.png`);
-  await file.save(imageBuffer, {
-    metadata: {
-      contentType: "image/png",
-      origin: ["*"],
-    }
-  });
-  await file.makePublic();
-  return file.publicUrl();
-}
-
-// Checks to see if the photo is already in Firebase storage.
-async function getAvatarFromCache (username) {
-  const storage = admin.storage().bucket(bucketId);
-  let file = storage.file(`${bucketPath}/${username}.png`);
-  let exists = await file.exists();
-  if (exists[0]) {
-    return { exists: true, url: file.publicUrl() };
-  } else {
-    return { exists: false, url: null };
-  }
-}
 
 // Store Instagram cookies in Firestore after login for use later.
 async function setSessionCache (cookie) {
