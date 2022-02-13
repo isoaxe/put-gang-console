910561e9a7633df4df04e0a6740cab7c5ce9a2ae
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-13 20:38:41 +0700

    helpers: Add monthName function
    Write a simple function that takes a number cast to string as a parameter in the format 'MM'. Then returns the name of the month associated with the number.

--- src/app/utils/helpers.js
+++ src/app/utils/helpers.js
@@ -67,7 +67,53 @@ export function numToCurrency (num) {
 
 // Capitalize the first letter of a given string.
 export function capitalize (string) {
-    return string[0].toUpperCase() + string.slice(1);
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+// Convert number cast as string to month name.
+export function monthName (number) {
+  let month;
+  switch (number) {
+    case "01":
+      month = "January";
+      break;
+    case "02":
+      month = "February";
+      break;
+    case "03":
+      month = "March";
+      break;
+    case "04":
+      month = "April";
+      break;
+    case "05":
+      month = "May";
+      break;
+    case "06":
+      month = "June";
+      break;
+    case "07":
+      month = "July";
+      break;
+    case "08":
+      month = "August";
+      break;
+    case "09":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+    case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
+    default:
+      month = "Error";
+  }
+  return month;
 }
 
 
