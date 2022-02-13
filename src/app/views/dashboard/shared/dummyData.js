7c8c0d44a0ff920ff291f1c780aac5176015349d
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-13 17:03:33 +0700

    Create dummyData.js
    This should be similar to what gets return in the api request. 12 objects, one for each month in the past year. Each one has a single figure attached for each of 4 stats.

---
+++ src/app/views/dashboard/shared/dummyData.js
@@ -0,0 +1,100 @@
+// Dummy data as would be returned from an api request.
+
+export const rawData = [
+  {"2021-03":
+    {
+      totalRevenues: 12300,
+      netRevenues: 10200,
+      joined: 73,
+      cancelled: 12,
+    }
+  },
+  {"2021-04":
+    {
+      totalRevenues: 11100,
+      netRevenues: 8200,
+      joined: 45,
+      cancelled: 9,
+    }
+  },
+  {"2021-05":
+    {
+      totalRevenues: 14700,
+      netRevenues: 10900,
+      joined: 56,
+      cancelled: 23,
+    }
+  },
+  {"2021-06":
+    {
+      totalRevenues: 15600,
+      netRevenues: 12100,
+      joined: 12,
+      cancelled: 54,
+    }
+  },
+  {"2021-07":
+    {
+      totalRevenues: 13700,
+      netRevenues: 9900,
+      joined: 55,
+      cancelled: 10,
+    }
+  },
+  {"2021-08":
+    {
+      totalRevenues: 14500,
+      netRevenues: 10800,
+      joined: 115,
+      cancelled: 34,
+    }
+  },
+  {"2021-09":
+    {
+      totalRevenues: 17400,
+      netRevenues: 13100,
+      joined: 36,
+      cancelled: 14,
+    }
+  },
+  {"2021-10":
+    {
+      totalRevenues: 18900,
+      netRevenues: 14600,
+      joined: 76,
+      cancelled: 44,
+    }
+  },
+  {"2021-11":
+    {
+      totalRevenues: 20300,
+      netRevenues: 16100,
+      joined: 66,
+      cancelled: 3,
+    }
+  },
+  {"2021-12":
+    {
+      totalRevenues: 23700,
+      netRevenues: 18100,
+      joined: 98,
+      cancelled: 43,
+    }
+  },
+  {"2021-01":
+    {
+      totalRevenues: 26500,
+      netRevenues: 21200,
+      joined: 71,
+      cancelled: 5,
+    }
+  },
+  {"2021-02":
+    {
+      totalRevenues: 28900,
+      netRevenues: 23200,
+      joined: 73,
+      cancelled: 12,
+    }
+  },
+]
