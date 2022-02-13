056b01d2781032fc6361ef2d9b6ca1d5f92e5ce2
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-13 11:01:43 +0700

    Charts: Implement AdvanceLineChart
    Import and implement the <AdvanceLineChart/> component as was done in <ApexCharts/>.
    These are all of the chart that are needed. May develop a way of altering how the data is displayed; e.g. discrete vs. cumulative etc.

--- src/app/views/dashboard/Charts.jsx
+++ src/app/views/dashboard/Charts.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { styled } from '@mui/system';
-import SimpleLineChart from './shared/SimpleLineChart';
+import { useTheme, styled } from '@mui/system';
+import { Card } from '@mui/material';
+import { H2 } from 'app/components/Typography'
 import SimpleCard from 'app/components/cards/SimpleCard';
+import SimpleLineChart from './shared/SimpleLineChart';
+import AdvanceLineChart from './shared/AdvanceLineChart';
 
 
 const Container = styled('div')(({ theme }) => ({
@@ -18,8 +21,24 @@ const Container = styled('div')(({ theme }) => ({
 }));
 
 const Charts = () => {
+    const { palette } = useTheme();
+    const textPrimary = palette.primary.main;
+
     return (
         <Container>
+            <Card sx={{ py: 2, pl: 1, pr: 2 }}>
+                <H2 sx={{ pl: 2 }}>Apex Line Chart</H2>
+                <AdvanceLineChart
+                    chartData={[
+                        {
+                            name: 'A',
+                            data: [40, 80, 20, 90, 145, 80, 125, 60, 120, 70],
+                        },
+                    ]}
+                    colors={[textPrimary]}
+                    height={300}
+                />
+            </Card>
             <SimpleCard title="simple line chart">
                 <SimpleLineChart />
             </SimpleCard>
