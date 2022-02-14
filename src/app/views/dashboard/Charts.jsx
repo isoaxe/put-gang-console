4c7462440d3332b3234faeee7840a3f7463f88f2
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-14 08:25:20 +0700

    Charts: Remove breadcrumb styling
    Charts are not making use of breadcrumb, so no need to retain styling either. This remained from the boilerplate that <Charts/> was based off.

--- src/app/views/dashboard/Charts.jsx
+++ src/app/views/dashboard/Charts.jsx
@@ -14,12 +14,6 @@ const Container = styled('div')(({ theme }) => ({
     [theme.breakpoints.down('sm')]: {
         margin: '16px',
     },
-    '& .breadcrumb': {
-        marginBottom: '30px',
-        [theme.breakpoints.down('sm')]: {
-            marginBottom: '16px',
-        },
-    },
 }));
 
 const FlexBox = styled(Box)(() => ({
