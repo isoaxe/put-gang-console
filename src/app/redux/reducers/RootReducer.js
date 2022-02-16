c6887e33dd931a0b12a868b91fee0c76117294eb
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-16 11:07:21 +0700

    RootReducer: Remove unused reducers
    The only reducer being used is for navigations. Remove the rest.

--- src/app/redux/reducers/RootReducer.js
+++ src/app/redux/reducers/RootReducer.js
@@ -1,14 +1,8 @@
 import { combineReducers } from 'redux'
-import ScrumBoardReducer from './ScrumBoardReducer'
-import NotificationReducer from './NotificationReducer'
-import EcommerceReducer from './EcommerceReducer'
 import NavigationReducer from './NavigationReducer'
 
 const RootReducer = combineReducers({
-    notifications: NotificationReducer,
     navigations: NavigationReducer,
-    scrumboard: ScrumBoardReducer,
-    ecommerce: EcommerceReducer,
 })
 
 export default RootReducer
