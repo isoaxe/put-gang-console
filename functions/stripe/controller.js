6aba5a3b80fd290c60cd6b22527046aa899490b0
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 18:19:11 +0700

    controller: Remove createPaymentsIntent from 'stripe'
    Remove the createPaymentsIntent function as it's no longer required on the server.

--- functions/stripe/controller.js
+++ functions/stripe/controller.js
@@ -6,29 +6,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_TEST, {
 	apiVersion: "2020-08-27"
 });
 
-// Create a payment intent and return secret to client.
-export async function createPaymentsIntent (req, res) {
-	const { membLvl } = req.body;
-	function calculateOrderAmount (membLvl) {
-		if (membLvl === "watch") return 5000; // Amount in ¢.
-		if (membLvl === "join") return 15000;
-	}
-	try {
-		// Create a paymentIntent with the order amount and currency
-		const paymentIntent = await stripe.paymentIntents.create({
-			amount: calculateOrderAmount(membLvl),
-			currency: "usd",
-			automatic_payment_methods: {
-				enabled: true,
-			},
-		});
-		res.send({ clientSecret: paymentIntent.client_secret });
-	} catch (err) {
-		return handleError(res, err);
-	}
-}
-
-
 // Create a new customer in Stripe. Required for subscription payments.
 export async function createCustomer (req, res) {
 	try {
