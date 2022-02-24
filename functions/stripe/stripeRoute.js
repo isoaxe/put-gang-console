dbdfc71f3a757c7dc4234b2b279e4fbbc82fc959
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 18:17:20 +0700

    stripeRoute: Remove route to /payments
    Delete the route to the /payments endpoint and the execution of the createPaymentsIntent function.

--- functions/stripe/stripeRoute.js
+++ functions/stripe/stripeRoute.js
@@ -1,12 +1,7 @@
-import { createPaymentsIntent, createCustomer, createSubscription } from "./controller.js";
+import { createCustomer, createSubscription } from "./controller.js";
 
 
 export default function stripeRoute (app) {
-	// Create a payments intent.
-	// This is required to get a client secret and initialize Stripe Elements api.
-	app.post("/stripe/payments",
-		createPaymentsIntent
-	);
 	// Create a new customer in Stripe.
 	app.post("/stripe/customer",
 		createCustomer
