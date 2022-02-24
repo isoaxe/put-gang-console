a1a62f567d7bd10bae497964feffb7fe348e2176
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 16:42:01 +0700

    controller: Add createSubscription function
    Write a function that calls the stripe api and creates a new subscription for a given customer.

--- functions/stripe/controller.js
+++ functions/stripe/controller.js
@@ -47,6 +47,29 @@ export async function createCustomer (req, res) {
 }
 
 
+// Create a new subscription in Stripe.
+export async function createSubscription (req, res) {
+	try {
+		const { priceId, customerId } = req.body;
+		const subscription = await stripe.subscriptions.create({
+			customer: customerId,
+			items: [{
+				price: priceId,
+			}],
+			payment_behavior: "default_incomplete",
+			expand: ["latest_invoice.payment_intent"],
+		});
+
+		res.send({
+      subscriptionId: subscription.id,
+      clientSecret: subscription.latest_invoice.payment_intent.client_secret,
+    });
+	} catch (err) {
+		return handleError(res, err);
+	}
+}
+
+
 // Standard error helper function.
 function handleError (res, err) {
 	return res.status(500).send({ error: `${err}` });
