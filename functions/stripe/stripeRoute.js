d3ba9e8b1d871e6625d98ed9168fbdbb3e850230
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 17:59:53 +0700

    stripeRoute: Add route for createSubscription
    Add a new route for the /subscription endpoint that runs the createSubscription function.

--- functions/stripe/stripeRoute.js
+++ functions/stripe/stripeRoute.js
@@ -1,4 +1,4 @@
-import { createPaymentsIntent, createCustomer } from "./controller.js";
+import { createPaymentsIntent, createCustomer, createSubscription } from "./controller.js";
 
 
 export default function stripeRoute (app) {
@@ -10,5 +10,9 @@ export default function stripeRoute (app) {
 	// Create a new customer in Stripe.
 	app.post("/stripe/customer",
 		createCustomer
+	);
+	// Create a new subscription in Stripe.
+	app.post("/stripe/subscription",
+		createSubscription
 	);
 }
