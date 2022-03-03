6b718853284516865301f0b421ff6e6b0c71df79
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-03 21:02:29 +0700

    stripeRoute: Add route for subscriptionPayment
    Add a new route at the /subscription-payment endpoint that runs the subscriptionPayment function from the previous commit when reached.

--- functions/stripe/stripeRoute.js
+++ functions/stripe/stripeRoute.js
@@ -1,4 +1,4 @@
-import { createCustomer, createSubscription } from "./controller.js";
+import { createCustomer, createSubscription, subscriptionPayment } from "./controller.js";
 
 
 export default function stripeRoute (app) {
@@ -9,5 +9,9 @@ export default function stripeRoute (app) {
 	// Create a new subscription in Stripe.
 	app.post("/stripe/subscription",
 		createSubscription
+	);
+	// Webhook for when customer is re-billed by Stripe.
+	app.post("/stripe/webhook/subscription-payment",
+		subscriptionPayment
 	);
 }
