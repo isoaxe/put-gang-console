51e18068a228eacf64d13b70c162dce6a8311b4c
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-06 22:04:43 +0700

    constants: Remove public test keys from client
    No need to hold the publishable test keys for test and live mode here. They are only required in put-gang-landing where the Elements api is initialized.

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -8,7 +8,3 @@ export const topBarHeight = 64
 export const sideNavWidth = 260
 
 export const ADMIN_EMAIL = "phillymantis@gmail.com";
-
-// Stripe API details.
-export const STRIPE_PUBLIC_KEY_TEST = "pk_test_K8ZJXsFTntA75GGQDTMmAZrX";
-export const STRIPE_PUBLIC_KEY_LIVE = "pk_live_krfnainmAevhtqlIRYMNfY5Y";
