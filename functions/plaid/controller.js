1aaa7095994aa2b69bbd60dec0f6fec8297ecf39
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-15 18:18:58 +0700

    controller: Rewrite createLinkToken with modern libs
    Was using the legacy library in the previous attempt at implementation. This was failing when trying to access the environments method on the plaid object during client initialization.
    Now reconfigured to make use of the current libraries.

--- functions/plaid/controller.js
+++ functions/plaid/controller.js
@@ -1,34 +1,38 @@
-import plaid from "plaid";
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
 import { PLAID_CLIENT_ID } from "./../util/constants.js";
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV],
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+      "PLAID-SECRET": process.env.PLAID_SECRET_KEY_SANDBOX,
+    },
+  },
+});
+
+const client = new PlaidApi(configuration);
 
 // Create a link_token for initialization of Plaid Link.
 export async function createLinkToken(req, res) {
+  // TODO: Can I add Firebase uid later?
+  const clientUserId = "Firebase uid here...";
+
+  const request = {
+    user: {
+      // This should correspond to a unique id for the current user.
+      client_user_id: clientUserId,
+    },
+    client_name: "Put Gang",
+    products: ["auth"],
+    language: "en",
+    webhook: "https://webhook.example.com",
+    country_codes: ["US"],
+  };
   try {
-    const plaidClient = new plaid.Client(
-      PLAID_CLIENT_ID,
-      process.env.PLAID_SECRET_KEY_SANDBOX,
-      plaid.environments.sandbox
-    );
+    const createTokenResponse = await client.linkTokenCreate(request);
 
-    // TODO: Can I add Firebase uid later?
-    const clientUserId = "Firebase uid here...";
-
-    plaidClient.createLinkToken(
-      {
-        user: {
-          client_user_id: clientUserId,
-        },
-        client_name: "Put Gang",
-        products: ["auth"],
-        country_codes: ["US"],
-        language: "en",
-        webhook: "https://sample.webhook.com",
-      },
-      function (error, linkTokenResponse) {
-        // Pass the result to your client-side app to initialize Link
-        res.json({ link_token: linkTokenResponse.link_token });
-      }
-    );
+    res.json(createTokenResponse.data);
   } catch (err) {
     return handleError(res, err);
   }
