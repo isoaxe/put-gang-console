a2a75428f492298a1c137d0525e108190105563d
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-16 10:57:03 +0700

    plaidRoute: Add route for token exchange
    Add an endpoint that runs the exchangeToken function from the previous commit.

--- functions/plaid/plaidRoute.js
+++ functions/plaid/plaidRoute.js
@@ -1,6 +1,8 @@
-import { createLinkToken } from "./controller.js";
+import { createLinkToken, exchangeTokens } from "./controller.js";
 
 export default function plaidRoute(app) {
   // Create a link_token for initialization of Plaid Link.
   app.post("/plaid/create-link-token", createLinkToken);
+  // Exchange a public_token for an access_token.
+  app.post("/plaid/exchange-tokens", exchangeTokens);
 }
