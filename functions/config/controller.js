9dd27e2cfab02fcb0bd66cf4f66823d84d452326
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-28 11:31:57 +0700

    controller: Add setPaymentOptions function
    Write a function that takes a paymentChoices boolean from the client and saves the value to Firestore.
    Returns a 201 response to the client to indicate that the request was successful.

--- functions/config/controller.js
+++ functions/config/controller.js
@@ -13,6 +13,20 @@ export async function getPaymentConfig(req, res) {
   }
 }
 
+// Specify whether card payment option should be shown to user.
+export async function setPaymentOptions(req, res) {
+  try {
+    const { paymentChoices } = req.body;
+    const db = admin.firestore();
+    const config = await db.collection("config").doc("config");
+    config.set({ paymentChoices }, { merge: true });
+
+    res.status(201).send({ success: "Payment options toggled" });
+  } catch (err) {
+    return handleError(res, err);
+  }
+}
+
 // Standard error helper function.
 function handleError(res, err) {
   return res.status(500).send({ error: `${err}` });
