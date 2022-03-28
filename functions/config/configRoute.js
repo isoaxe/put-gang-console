6d5dd2e18dd24a4f43afd8bb089e811dd602aedc
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-28 11:37:43 +0700

    configRoute: Add routing for setPaymentOptions
    Add routing to the same /payment-options endpoint for POST requests. This runs the setPaymentOptions function from the second last commit.

--- functions/config/configRoute.js
+++ functions/config/configRoute.js
@@ -1,6 +1,8 @@
-import { getPaymentConfig } from "./controller.js";
+import { getPaymentConfig, setPaymentOptions } from "./controller.js";
 
 export default function configRoute(app) {
   // Check what payment options should be displayed in ChoiceModal.
   app.get("/config/payment-options", getPaymentConfig);
+  // Specify if card payment option should be shown to the user.
+  app.post("/config/payment-options", setPaymentOptions);
 }
