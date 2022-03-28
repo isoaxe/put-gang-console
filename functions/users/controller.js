701e7746f64b4b6dfa353e951521fafa7d4cd5f3
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-28 21:04:09 +0700

    controller: Do not send json for edit user function
    When status is 204, cannot send JSON in response as the protocol does not support this. Can only send status.

--- functions/users/controller.js
+++ functions/users/controller.js
@@ -216,7 +216,7 @@ export async function edit(req, res) {
         return res.status(404).end("not found");
       }
     }
-    return res.status(204).send({ message: "User successfully edited." });
+    return res.sendStatus(204);
   } catch (err) {
     return handleError(res, err);
   }
