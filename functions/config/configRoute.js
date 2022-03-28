a08c7911f85bbd1811f8ce9bcedc38391fd449f4
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-28 15:45:40 +0700

    configRoute: Add auth for PATCH request
    + Import authentication and authorization functions.
    + Implement with the PATCH request at the /payment-options endpoint. Only admin user has permission.

--- functions/config/configRoute.js
+++ functions/config/configRoute.js
@@ -1,8 +1,15 @@
 import { getConfigData, setPaymentOptions } from "./controller.js";
+import { isAuthenticated } from "./../auth/authenticated.js";
+import { isAuthorized } from "./../auth/authorized.js";
 
 export default function configRoute(app) {
   // Get all configuration data.
   app.get("/config/all", getConfigData);
   // Specify if card payment option should be shown to the user.
-  app.patch("/config/payment-options", setPaymentOptions);
+  app.patch(
+    "/config/payment-options",
+    isAuthenticated,
+    isAuthorized({ hasRole: ["admin"] }),
+    setPaymentOptions
+  );
 }
