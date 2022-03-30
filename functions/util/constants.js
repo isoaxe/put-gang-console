a1829f8308170f1d8f5c88c08eccdc5c3d39a281
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-30 20:50:10 +0700

    constants: Add more Discord ids
    Add ids for the 'Gangsta' and 'Super Gangsta' role in Discord.

--- functions/util/constants.js
+++ functions/util/constants.js
@@ -16,3 +16,5 @@ export const PLAID_CLIENT_ID = "622f07f6f80bde0013a73f27";
 // Discord credentials for bot.
 export const DISCORD_CLIENT_ID = "958210056388763658";
 export const DISCORD_GUILD_ID = "943724894594691123";
+export const GANGSTA_ID = "943736283447050241";
+export const SUPER_GANGSTA_ID = "943738474715033633";
