87554718a9dc771ff3d8b4af5f34665077dc0693
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-30 11:28:58 +0700

    discordRoute: Update http verb
    Needs to be a POST request rather than a GET request according the Discord developer portal.

--- functions/discord/discordRoute.js
+++ functions/discord/discordRoute.js
@@ -2,5 +2,5 @@ import { role } from "./controller.js";
 
 export default function discordRoute(app) {
   // Assign or remove the appropriate role to requesting user.
-  app.get("/discord/role", role);
+  app.post("/discord/role", role);
 }
