071492cf7fb5e62d67a50b83565699486054c5d4
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-04-01 21:08:16 +0700

    discordRoute: Change http verb to get
    Make the route handle a GET request instead of POST. See previous commit.

--- functions/discord/discordRoute.js
+++ functions/discord/discordRoute.js
@@ -2,5 +2,5 @@ import { role } from "./controller.js";
 
 export default function discordRoute(app) {
   // Assign or remove the appropriate role to requesting user.
-  app.post("/discord/role", role);
+  app.get("/discord/role", role);
 }
