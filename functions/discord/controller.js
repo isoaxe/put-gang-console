cf99a9eb74cfad30d2e9502a1316416b87f8a8ba
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-04-02 08:45:24 +0700

    controller: Move discord login to top
    Move the function that logs into Discord to right under where it is declared.

--- functions/discord/controller.js
+++ functions/discord/controller.js
@@ -10,13 +10,13 @@ export async function role(req, res) {
       intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
     });
 
+    // Login to Discord with your client's token
+    client.login(process.env.DISCORD_SECRET_TOKEN);
+
     // When the client is ready, run this code (only once)
     client.once("ready", () => {
       console.log("Ready!");
     });
-
-    // Login to Discord with your client's token
-    client.login(process.env.DISCORD_SECRET_TOKEN);
 
     client.on("interactionCreate", async (interaction) => {
       if (!interaction.isCommand()) return;
