94ce2185769ae1109126064617839fa3ff900f15
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-04-09 16:28:58 +0700

    stripeRoute: Add routing for changePaymentType
    Add a new /payment-type endpoint that runs the changePaymentType function.

--- functions/stripe/stripeRoute.js
+++ functions/stripe/stripeRoute.js
@@ -1,6 +1,7 @@
 import {
   createCustomer,
   createSubscription,
+  changePaymentType,
   achPayment,
   subscriptionPayment,
 } from "./controller.js";
@@ -10,6 +11,8 @@ export default function stripeRoute(app) {
   app.post("/stripe/customer", createCustomer);
   // Create a new subscription in Stripe.
   app.post("/stripe/subscription", createSubscription);
+  // Modify a subscription payment method type in Stripe.
+  app.patch("/stripe/payment-type", changePaymentType);
   // Create a new ACH back payment in Stripe.
   app.post("/stripe/payment", achPayment);
   // Webhook for when customer is re-billed by Stripe.
