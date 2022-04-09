ab0afadbf0cdbbceeff8aa8c51de438a2cbf2848
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-04-09 21:22:29 +0700

    stripeRoute: Remove payment-type endpoint
    Remove the routing for the /payment-type endpoint that runs the changePaymentType function.

--- functions/stripe/stripeRoute.js
+++ functions/stripe/stripeRoute.js
@@ -1,7 +1,6 @@
 import {
   createCustomer,
   createSubscription,
-  changePaymentType,
   achPayment,
   subscriptionPayment,
 } from "./controller.js";
@@ -11,8 +10,6 @@ export default function stripeRoute(app) {
   app.post("/stripe/customer", createCustomer);
   // Create a new subscription in Stripe.
   app.post("/stripe/subscription", createSubscription);
-  // Modify a subscription payment method type in Stripe.
-  app.patch("/stripe/payment-type", changePaymentType);
   // Create a new ACH back payment in Stripe.
   app.post("/stripe/payment", achPayment);
   // Webhook for when customer is re-billed by Stripe.
