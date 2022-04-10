eedeadeb890bb4caacbd97e7fa7f6cc7361e7d04
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2022-04-10 16:18:10 +0600

    Add redirect url

--- functions/util/constants.js
+++ functions/util/constants.js
@@ -19,4 +19,10 @@ const RemoteOauth = "https://discord.com/api/oauth2/authorize?client_id=96175408
 const LocalOauth = "https://discord.com/api/oauth2/authorize?client_id=961754087341305876&redirect_uri=http%3A%2F%2Flocalhost%3A5001%2Fput-gang%2Fus-central1%2Fapi%2Fdiscord%2F&response_type=code&scope=guilds.join%20identify"
 
 export const OAUTH_URL = 
-  window.location.hostname === "localhost" ? LocalOauth : RemoteOauth;
+  MODE === "test" ? LocalOauth : RemoteOauth;
+
+const localRedUrl = "http://localhost:5001/put-gang/us-central1/api";
+const remoteRedUrl = "https://us-central1-put-gang.cloudfunctions.net/api";
+
+export const REDIRECT_URL =
+  MODE === "test" ? localRedUrl : remoteRedUrl;
