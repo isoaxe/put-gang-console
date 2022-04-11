f3803f56e46a6cfcd20c0e44651a9f7bb45dba59
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-04-11 11:24:50 +0700

    joinGuild: Run prettier
    Auto format on save.

--- functions/discord/joinGuild.js
+++ functions/discord/joinGuild.js
@@ -1,23 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 
 export const JoinGUild = (_token, _guild, _user, roles) => {
-    return new Promise(async (resolve, reject) => {
-        if(!_token){ return reject(new Error("No Token Provided")) }
-        axios({
-            method: "put",
-            url: `https://discord.com/api/v9/guilds/${_guild}/members/${_user}`,
-            headers: {
-                authorization: `Bot ${process.env.BOT_TOKEN}`,
-                "Content-Type": "application/json"
-            },
-            data: {
-                access_token: _token,
-                roles: roles || []
-            }
-        }).then(d => {
-            resolve(d)
-        }).catch(err => {
-            reject(err)
-        })
+  return new Promise(async (resolve, reject) => {
+    if (!_token) {
+      return reject(new Error("No Token Provided"));
+    }
+    axios({
+      method: "put",
+      url: `https://discord.com/api/v9/guilds/${_guild}/members/${_user}`,
+      headers: {
+        authorization: `Bot ${process.env.BOT_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        access_token: _token,
+        roles: roles || [],
+      },
     })
-}
+      .then((d) => {
+        resolve(d);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
